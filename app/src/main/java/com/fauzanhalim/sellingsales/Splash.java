1b315b61515b10f5fb450ab8ce22b07afbab8cb5
fauzanhalim <nurfauzanhalim@gmail.com>
2022-08-12 10:13:58 +0700

    first commit

---
+++ app/src/main/java/com/fauzanhalim/sellingsales/Splash.java
@@ -0,0 +1,30 @@
+package com.fauzanhalim.sellingsales;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Thread myThread = new Thread()
+        {
+            @Override
+            public void run() {
+                try {
+                    sleep(4200);
+                    Intent intent = new Intent(getApplicationContext(),Welcome.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+}
